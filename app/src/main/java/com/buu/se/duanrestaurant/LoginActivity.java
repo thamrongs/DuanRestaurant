bc14e68f470e47a848876270f91cdfa5c0ee1935
Sarin <Sarin.Kesphanich@gmail.com>
2015-03-22 15:55:32 +0700

    Sarin edit LoginActivity

--- app/src/main/java/com/buu/se/duanrestaurant/LoginActivity.java
+++ app/src/main/java/com/buu/se/duanrestaurant/LoginActivity.java
@@ -2,9 +2,13 @@ package com.buu.se.duanrestaurant;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -14,16 +18,26 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 
 public class LoginActivity extends Activity {
 
-    private String username, password, ip;
+    private String username, password, ip, fullurl;
     private EditText user, pass, input;
     private  AlertDialog.Builder alertDlg;
     private AlertDialog alert;
+    EditText etResponse;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -110,16 +124,94 @@ public class LoginActivity extends Activity {
     }
 
     public void onClickLogin(View v){
-        Intent data = new Intent(LoginActivity.this, MainActivity.class);
-        username = user.getText().toString();
-        password = pass.getText().toString();
+        if(isOnline()) {
+            Intent data = new Intent(LoginActivity.this, MainActivity.class);
+            username = user.getText().toString();
+            password = pass.getText().toString();
 
-        if(!username.matches("") && !password.matches("")) {
+            if (!username.matches("") && !password.matches("")) {
+                // call AsynTask to perform network operation on separate thread
+                fullurl = "http://" + ip + "/resman/index.php/authen/login/" + username + "/" + password;
+                String ii = GET(fullurl);
+                Toast.makeText(getApplicationContext(), ii, Toast.LENGTH_LONG).show();
 
-        }else {
-            Toast.makeText(getApplicationContext(), "Please fill the form, don't leave any field blank", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(getApplicationContext(), "Please fill the form, don't leave any field blank", Toast.LENGTH_LONG).show();
+            }
+            data.putExtra("userid", "000001");
+            data.putExtra("username", username);
+//        data.putExtra("username", username);
+            startActivity(data);
+        } else {
+            Toast.makeText(getApplicationContext(), "Network is Disconnect", Toast.LENGTH_LONG).show();
         }
-        data.putExtra("userid","000001");
-        startActivity(data);
+    }
+
+    public boolean isOnline() {
+        ConnectivityManager cm =
+                (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+        return netInfo != null && netInfo.isConnectedOrConnecting();
+    }
+
+    public String GET(String url){
+//        Toast.makeText(getApplicationContext(), url, Toast.LENGTH_LONG).show();
+//        user.setText(url);
+        InputStream inputStream = null;
+        String result = "";
+        try {
+
+            // create HttpClient
+            HttpClient httpclient = new DefaultHttpClient();
+
+            // make GET request to the given URL
+            HttpResponse httpResponse = httpclient.execute(new HttpGet(url));
+
+            // receive response as inputStream
+            inputStream = httpResponse.getEntity().getContent();
+
+            // convert inputstream to string
+            if(inputStream != null) {
+                result = convertInputStreamToString(inputStream);
+            }
+            else {
+                result = "Did not work!";
+            }
+
+
+        } catch (Exception e) {
+            //Log.d("InputStream", e.getLocalizedMessage());
+            //Log.d("ssss", e.getMessage());
+            result = "Exception";
+        }
+        Toast.makeText(getApplicationContext(), result, Toast.LENGTH_LONG).show();
+        return result;
+    }
+
+    private static String convertInputStreamToString(InputStream inputStream) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader( new InputStreamReader(inputStream));
+        String line = "";
+        String result = "";
+        while((line = bufferedReader.readLine()) != null)
+            result += line;
+
+        inputStream.close();
+        return result;
+
+    }
+
+    private class HttpAsyncTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+
+            return GET(urls[0]);
+        }
+        // onPostExecute displays the results of the AsyncTask.
+        @Override
+        protected void onPostExecute(String result) {
+//            Toast.makeText(getBaseContext(), "Received!", Toast.LENGTH_LONG).show();
+            //etResponse.setText(result);
+            Toast.makeText(getApplicationContext(), "Received!", Toast.LENGTH_LONG).show();
+        }
     }
 }
