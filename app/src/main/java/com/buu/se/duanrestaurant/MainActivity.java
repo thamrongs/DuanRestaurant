da720bc7d980305df50097a0e4e7a3c4aa948c7d
yniarnof <itthiphat55160554@gmail.com>
2015-03-22 14:44:24 +0700

    llll

--- app/src/main/java/com/buu/se/duanrestaurant/MainActivity.java
+++ app/src/main/java/com/buu/se/duanrestaurant/MainActivity.java
@@ -4,6 +4,9 @@ import android.app.Activity;
 
 import android.app.ActionBar;
 import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.content.Context;
@@ -20,8 +23,12 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.support.v4.widget.DrawerLayout;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import java.util.Calendar;
 
 
 public class MainActivity extends Activity
@@ -164,6 +171,30 @@ public class MainActivity extends Activity
         AlertDialog alert = alertDlg.create();
         alert.show();
 
+    }
+    public class DatePickerFragment extends DialogFragment
+            implements DatePickerDialog.OnDateSetListener {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+
+
+            return new DatePickerDialog(getActivity(), this, year, month, day);
+        }
+
+        public void onDateSet(DatePicker view, int year, int month, int day) {
+            Button showdate = (Button) findViewById(R.id.showdate);
+            showdate.setText(day+"/"+month+"/"+year);
+        }
+    }
+    public void showDatePickerDialog(View v) {
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getFragmentManager(), "datePicker");
     }
 
 
