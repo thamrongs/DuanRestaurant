b3ab8632f242e1c51091716beb5da2a39f1c11e2
adchariya <phraewz.55160406@gmail.com>
2015-03-31 12:00:00 +0700

    jjj

--- app/src/main/java/com/buu/se/duanrestaurant/ReserveTable.java
+++ app/src/main/java/com/buu/se/duanrestaurant/ReserveTable.java
@@ -1,10 +1,19 @@
 package com.buu.se.duanrestaurant;
 
 import android.app.Activity;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.format.DateFormat;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
 
 /**
  * Created by thamrongs on 3/9/15 AD.
@@ -23,5 +32,44 @@ public class ReserveTable extends Activity {
         textView.setText(id);
         Log.d("ID", id+"");
 
+    }
+
+    public class TimePickerFragment extends DialogFragment
+            implements TimePickerDialog.OnTimeSetListener {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current time as the default values for the picker
+            final Calendar c = Calendar.getInstance();
+            int hour = c.get(Calendar.HOUR_OF_DAY);
+            int minute = c.get(Calendar.MINUTE);
+
+            // Create a new instance of TimePickerDialog and return it
+            return new TimePickerDialog(getActivity(), this, hour, minute,
+                    DateFormat.is24HourFormat(getActivity()));
+
+        }
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+            //MainActivity.this.SetButton(view,hourOfDay,minute);
+            Button tButton = (Button) findViewById(R.id.imageButton3);
+            String m = "",h = "";
+            if(minute < 10){
+                m = "0"+minute;
+            }else{
+                m = minute+"";
+            }
+            if(hourOfDay < 10){
+                h = "0"+hourOfDay;
+            }else{
+                h = hourOfDay+"";
+            }
+            tButton.setText(h + ":" + m);
+        }
+
+    } // time picker
+    public void showTimePickerDialog(View v) {
+
+        DialogFragment newFragment = new TimePickerFragment();
+        newFragment.show(getFragmentManager(), "timePicker");
     }
 }
